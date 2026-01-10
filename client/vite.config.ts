f6f373ae4942278e4d24230a0f392703127881bc
Letlhogonolo Rakgantsho <107153213+Eaziey@users.noreply.github.com>
2026-01-10 18:28:16 +0000

    made base url dynamic for prod and dev environment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+const isProd =process.env.NODE_ENV ==='production'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/GitHub_Commit_Explorer/'
+  base: isProd? '/GitHub_Commit_Explorer/': '/'
 })
